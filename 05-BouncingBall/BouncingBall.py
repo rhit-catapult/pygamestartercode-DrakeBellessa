c8fd168fdd41c41d3b934206a7ba33cdc8a2e8f9
Drake Bellessa <drakebellessa@gmail.com>
2024-06-07 17:40:50 -0400

    Finished

--- 05-BouncingBall/BouncingBall.py
+++ 05-BouncingBall/BouncingBall.py
@@ -5,30 +5,82 @@ import random
 
 # You will implement this module ENTIRELY ON YOUR OWN!
 
-# TODO: Create a Ball class.
-# TODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
-# TODO: Methods: __init__, draw, move
+# DONE: Create a Ball class.
+# DONE: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
+# DONE: Methods: __init__, draw, move
+
+class Ball:
+    def __init__(self, screen, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.speed_x = random.randint(5, 8)
+        self.speed_y = random.randint(5, 8)
+        self.radius = random.randint(5, 45)
+        self.color = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.y = self.y + self.speed_y
+        self.x = self.x + self.speed_x
+        if self.y > self.screen.get_height() + self.radius or self.y < 0 + self.radius:
+            self.speed_y = -1*self.speed_y
+        if self.x > self.screen.get_width() + self.radius or self.x < 0 + self.radius:
+            self.speed_x = -1*self.speed_x
+        if self.x > self.screen.get_width() - self.radius or self.x < 0 - self.radius:
+            self.speed_x = -1*self.speed_x
+        if self.y > self.screen.get_width() - self.radius or self.y < 0 - self.radius:
+            self.speed_y = -1*self.speed_y
+
+
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((300, 300))
+    screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption('Bouncing Ball')
     screen.fill(pygame.Color('gray'))
     clock = pygame.time.Clock()
 
-    # TODO: Create an instance of the Ball class called ball1
+    # DONE: Create an instance of the Ball class called ball1
+    ball1 = Ball(screen, 300, 50)
+
+    list = []
+    list.append(ball1)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            pressed_keys = pygame.key.get_pressed()
+            if pressed_keys[pygame.K_SPACE]:
+                    ball1.speed_x = 0*ball1.speed_x
+                    ball1.speed_y = 0*ball1.speed_y
+            if pressed_keys[pygame.K_LSHIFT]:
+                    ball1.speed_x = random.randint(5, 8) + ball1.speed_x
+                    ball1.speed_y = random.randint(5, 8) + ball1.speed_y
+            if pressed_keys[pygame.K_LEFT]:
+                list.append(Ball(screen,random.randint(10, 580), random.randint(10, 580)))
+        screen.fill(pygame.Color('gray'))
+        for ball in list:
+            ball.move()
+        for ball in list:
+            ball.draw()
+
 
         clock.tick(60)
-        screen.fill(pygame.Color('gray'))
 
-        # TODO: Move the ball
-        # TODO: Draw the ball
+
+    # DONE: Move the ball
+    # DONE: Draw the ball
+        ball1.draw()
+        ball1.move()
+
+
+
 
         pygame.display.update()
 
